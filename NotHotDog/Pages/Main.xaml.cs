89e748f67ab224036e1381b50557c08258174111
Allen Zhong <zhong.allen@gmail.com>
2017-06-12 16:20:08 +1200

    add tab page

---
+++ NotHotDog/Pages/Main.xaml.cs
@@ -0,0 +1,15 @@
+﻿﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace NotHotDog
+{
+    public partial class Main : TabbedPage
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
