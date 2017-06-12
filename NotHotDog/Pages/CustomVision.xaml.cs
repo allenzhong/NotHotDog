e808319fa932915d6c3e2bcc335d423c3b41485c
Allen Zhong <zhong.allen@gmail.com>
2017-06-12 21:00:16 +1200

    camera features

--- NotHotDog/Pages/CustomVision.xaml.cs
+++ NotHotDog/Pages/CustomVision.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
 
 namespace NotHotDog
 {
@@ -11,5 +13,33 @@ namespace NotHotDog
         {
             InitializeComponent();
         }
+
+		private async void loadCamera(object sender, EventArgs e)
+		{
+			await CrossMedia.Current.Initialize();
+
+			if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+			{
+				await DisplayAlert("No Camera", ":( No camera available.", "OK");
+				return;
+			}
+
+			MediaFile file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+			{
+				PhotoSize = PhotoSize.Medium,
+				Directory = "Sample",
+				Name = $"{DateTime.UtcNow}.jpg"
+			});
+
+			if (file == null)
+				return;
+
+			image.Source = ImageSource.FromStream(() =>
+			{
+				return file.GetStream();
+			});
+
+			file.Dispose();
+		}
     }
 }
