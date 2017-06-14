5d8599c1976d37b1b5fdb0e16cb7ae76271f5ce0
Allen Zhong <zhong.allen@gmail.com>
2017-06-14 13:52:04 +1200

    add predition

--- NotHotDog/Pages/CustomVision.xaml.cs
+++ NotHotDog/Pages/CustomVision.xaml.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
-
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 using Xamarin.Forms;
 using Plugin.Media;
 using Plugin.Media.Abstractions;
+using System.Linq;
 
 namespace NotHotDog
 {
@@ -39,7 +44,52 @@ namespace NotHotDog
 				return file.GetStream();
 			});
 
-			file.Dispose();
+			//file.Dispose();
 		}
+
+        static byte[] GetImageAsByteArray(MediaFile file)
+        {
+            var stream = file.GetStream();
+            BinaryReader binaryReader = new BinaryReader(stream);
+            return binaryReader.ReadByte((int)stream.Length);
+        }
+
+        async Task MakePredictionRequest(MediaFile file)
+        {
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Add("Predition-Key","306055b083f64c3ca25171b754c1f4b3");
+            string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/9b3a082f-147a-42ce-943d-e0853a1391c5/image?iterationId=a729604d-0622-4507-afac-77a6e643d754";
+
+            HttpResponseMessage response;
+
+            byte[] byteData = GetImageAsByteArray(file);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(url, content);
+
+                if(response.IsSuccessStatusCode) 
+                {
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    JObject rss = JObject.Parse(responseString);
+
+                    var probability = from p in rss["Predictions"] select (string)p["Probability"];
+                    var tag = from p in rss["Predictions"] select (string)p["Tag"];
+
+                    foreach (var item in tag)
+                    {
+                        TagLabel.Text += item + ": \n";
+                    }
+                    foreach (var item in probability)
+                    {
+                        PredictionLabel.Text += item + "\n";
+                    }
+                }
+
+                file.Dispose();
+            }
+        }
     }
 }
