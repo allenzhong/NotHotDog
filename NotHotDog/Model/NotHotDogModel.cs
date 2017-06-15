2bd4aa29862fb94d785d55a3abe7db913d656f08
Allen Zhong <zhong.allen@gmail.com>
2017-06-15 18:17:58 +1200

    add model

---
+++ NotHotDog/Model/NotHotDogModel.cs
@@ -0,0 +1,20 @@
+﻿﻿using System;
+using Newtonsoft.Json;
+
+namespace NotHotDog
+{
+    public class NotHotDogModel
+    {
+        [JsonProperty(PropertyName = "Id")]
+		public string ID { get; set; }
+
+        [JsonProperty(PropertyName = "Longitude")]
+		public float Longitude { get; set; }
+
+        [JsonProperty(PropertyName = "Latitude")]
+		public float Latitude { get; set; }
+
+        [JsonProperty(PropertyName = "City")]
+		public string City { get; set; }
+    }
+}
