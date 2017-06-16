e39f0752ac0a215c1af9eea0491e4eeb03064411
Allen Zhong <zhong.allen@gmail.com>
2017-06-16 14:39:38 +1200

    add azure manager

---
+++ NotHotDog/AzureManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+
+namespace NotHotDog
+{
+    public class AzureManager
+    {
+        private static AzureManager instance;
+        private MobileServiceClient client;
+        private IMobileServiceTable<NotHotDogModel> notHotDogTable;
+        private const string MOBILE_APP_URL = "";
+
+        private AzureManager()
+        {
+            this.client = new MobileServiceClient(MOBILE_APP_URL);
+            this.notHotDogTable = this.client.GetTable<NotHotDogModel>();
+        }
+
+        public MobileServiceClient AzureClient
+        {
+            get { return client; }
+        }
+
+        public static AzureManager AzureManagerInstance
+        {
+            get 
+            {
+                if(instance == null)
+                {
+                    instance = new AzureManager();
+                }
+
+                return instance;
+            }
+        }
+
+        public async Task<List<NotHotDogModel>> GetHotDogInformation()
+        {
+            return await this.notHotDogTable.ToListAsync();
+        }
+    }
+}
--- NotHotDog/Pages/AzureTable.xaml.cs
+++ NotHotDog/Pages/AzureTable.xaml.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Microsoft.WindowsAzure.MobileServices;
 using Xamarin.Forms;
 
 namespace NotHotDog
 {
     public partial class AzureTable : ContentPage
     {
+		MobileServiceClient client = AzureManager.AzureManagerInstance.AzureClient;
         public AzureTable()
         {
             InitializeComponent();
+        }
+
+        async void Handle_ClickedAsync(object sender, System.EventArgs e)
+        {
+            List<NotHotDogModel> notHotDogInformation = await AzureManager.AzureManagerInstance.GetHotDogInformation();
+            HotDogList.ItemsSource = notHotDogInformation;
         }
     }
 }
